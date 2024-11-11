a10f99915d55ef62cab612efd281a801eb9df9af
Mauricio Alarcón <110872845+Vjyhgv@users.noreply.github.com>
2024-11-11 09:42:19 -0300

    botones segun sesion listos. Ahora trabajar en conectar jugadores

--- src/components/landingPage/botonesLandingPage.jsx
+++ src/components/landingPage/botonesLandingPage.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../assets/styles/landingPage/botones.css"
 
+import { useContext } from "react";
+import { AuthContext } from "../../auth/AuthContext";
+
 function botones(){
 
     // para navegar entre rutas https://stackoverflow.com/questions/34735580/how-to-do-a-redirect-to-another-route-with-react-router
 
+    const { token } = useContext(AuthContext);
     const navigate = useNavigate();
+    
 
-    const handleclick = () =>{
+    const hacerLogin = () =>{
         navigate("/login")
     };
 
@@ -18,7 +23,27 @@ function botones(){
 
         <div className="contenedorBotones">
 
-            <button onClick={handleclick} id="IniciarSesionRegistrarse">Iniciar Sesion/Registrarse</button>
+            <div>
+                {(() => {   //ayuda chatgpt para mostrar si esta logueado o no
+                    if (token != "null") { //el token null que se guarda en localstorage es string
+
+                        return (
+                            <button onClick={() => {/* lógica para unirse a la partida */}}>
+                                Unirse a Partida
+                            </button>
+                        );
+                        
+                    } else {
+
+                        return (
+                            <button onClick={hacerLogin} id="IniciarSesionRegistrarse">Iniciar Sesion/Registrarse</button>
+                        );
+                    }
+                })()}
+            </div>
+                
+
+                {/* <button onClick={handleclick} id="IniciarSesionRegistrarse">Iniciar Sesion/Registrarse</button> */}
 
         </div>
 
