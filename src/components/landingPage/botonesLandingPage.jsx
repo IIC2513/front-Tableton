c7dd49d3c5498c88ae33aef86399d3ab187564cd
Mauricio Alarcón <110872845+Vjyhgv@users.noreply.github.com>
2024-11-12 17:18:31 -0300

    comprar y tirar dados funcionan

--- src/components/landingPage/botonesLandingPage.jsx
+++ src/components/landingPage/botonesLandingPage.jsx
@@ -19,6 +19,10 @@ function botones(){
         navigate("/login")
     };
 
+    const irPartida = () =>{
+        navigate("/partida")
+    };
+
     const cerrarSesion = () => { //Chat gpt me ayudo a cerrar la sesion con el removeTOken
         setToken(null); // Limpia el token en el contexto (esto dependerá de cómo manejes el token)
         localStorage.removeItem("token"); // Borra el token del localStorage
@@ -65,6 +69,7 @@ function botones(){
                 <>
                     <button onClick={crearJugador}>Unirse a Partida</button>
                     <button onClick={cerrarSesion}>Cerrar Sesión</button>
+                    <button onClick={irPartida}>Ir a partida</button>
                 </>
             ) : (
                 <button onClick={hacerLogin} id="IniciarSesionRegistrarse">Iniciar Sesion/Registrarse</button>
--- src/components/partida/IndexPartida.jsx
+++ src/components/partida/IndexPartida.jsx
@@ -2,12 +2,15 @@
 import Background from "../common/Background"
 import Board from "../common/Board"
 import "../../assets/styles/Board.css"
+import BarraTareas from "../common/barraTareas"
 
 function IndexPartida(){
     return (
         <div className="contenedorPadreInterfaz">
 
+        <BarraTareas></BarraTareas>
         <Background></Background>
+
 
             <div className="Contenedor_lista_tablero">
 
