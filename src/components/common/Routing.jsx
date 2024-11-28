cfd4eb9eb5d4e82c3059504d189a83c3bf1a20b4
Vjyhgv <mauricio.alarcn@uc.cl>
2024-11-28 10:15:37 -0300

    rutas protegidas

--- src/components/common/Routing.jsx
+++ src/components/common/Routing.jsx
@@ -6,6 +6,8 @@ import IndexPartida from "../partida/IndexPartida"
 import Nosotros from "../info/Nosotros"
 import Reglas from "../info/Reglas"
 import LandingPage from "../landingPage/LandingPage"
+import ProtectedRoute from "../../protectedRoutes/ProtectedRoutes.jsx";  //chatgpt para proteger rutas
+
 
 function Routing(){
     return(
@@ -14,13 +16,33 @@ function Routing(){
         
             <Routes>
 
-                <Route path={"/partida/:partidaId/espera"} element= {<Index></Index>}></Route>
                 <Route path={"/"} element = {<LandingPage></LandingPage>}></Route>
                 <Route path={"/login"} element= {<LoginPage></LoginPage>}></Route>
                 <Route path="/nosotros" element={<Nosotros></Nosotros>}></Route>
                 <Route path="/reglas" element={<Reglas></Reglas>}></Route>
-                <Route path={"/partida/:partidaId"} element= {<IndexPartida></IndexPartida>}></Route>
+                {/* <Route path={"/partida/:partidaId/espera"} element= {<Index></Index>}></Route> */}
+                {/* <Route path={"/partida/:partidaId"} element= {<IndexPartida></IndexPartida>}></Route> */}
 
+                {/* Rutas protegidas */}
+                <Route
+                    path="/partida/:partidaId"
+                    element={
+                        <ProtectedRoute>
+                            <IndexPartida />
+                        </ProtectedRoute>
+                    }
+                >
+                </Route>
+                <Route
+                    path="/partida/:partidaId/espera"
+                    element={
+                        <ProtectedRoute>
+                            <Index />
+                        </ProtectedRoute>
+                    }
+                >
+                </Route>
+        
             </Routes>
         
         </BrowserRouter>
